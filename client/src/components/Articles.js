a7244b1ee80019eab13f7bb2dc202735408123c7
Cook1619 <cook6348@gmail.com>
2019-01-31 12:59:58 -0600

    Testing the await syntax

--- client/src/components/Articles.js
+++ client/src/components/Articles.js
@@ -2,22 +2,38 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Article extends Component {
+    constructor(props) {
+        super(props);
 
-    fetchArticles = () => {
-        axios.get('http://localhost:3001/articles')
-            .then(res => console.log(res.data))
-            
-            .catch(err => console.log(err));
+        this.state = {
+            articles: []
+        }
+    }
+
+    // fetchArticles = () => {
+    //     axios.get('http://localhost:3001/articles')
+    //         .then(res => console.log(res.data))
+    //         .catch(err => console.log(err));
+    // }
+    fetchArticles = async () => {
+        try {
+            let res = await axios.get('http://localhost:3001/articles');
+            this.setState({
+                articles: res.data
+            });
+        } catch (e) {
+            console.log(e);
+        }
     }
     componentDidMount() {
         this.fetchArticles();
     }
 
     render() {
-        console.log()
+        console.log(this.state)
         return (
             <div>
-            Hello WOrld!
+                Hello WOrld!
             </div>
         )
     }
